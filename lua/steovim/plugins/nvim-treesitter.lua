47670a845d80fdab86993d05834b5d80c13d8902
Steyn Huurman <git@steynhuurman.nl>
2023-05-01 16:45:50 +0800

    feat: improve theme and treesitter

--- lua/steovim/plugins/nvim-treesitter.lua
+++ lua/steovim/plugins/nvim-treesitter.lua
@@ -1,4 +1,14 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
-	config = function() vim.cmd(":TSUpdate") end
+	config = function() 
+        require('nvim-treesitter.configs').setup({
+            highlight = { 
+                enable = true, 
+                  disable = { "lua" },
+            },
+            incremental_selection = { enable = true },
+            textobjects = { enable = true },
+        })
+        vim.cmd(":TSUpdate")
+    end
 }
