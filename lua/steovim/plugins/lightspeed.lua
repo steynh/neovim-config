42bdecb61428c672c2fbe7eb99dcd3a5151f3614
Steyn Huurman <git@steynhuurman.nl>
2023-09-02 21:16:40 +0800

    feat: disable multi-line lightspeed jump for f/F/t/T

--- lua/steovim/plugins/lightspeed.lua
+++ lua/steovim/plugins/lightspeed.lua
@@ -1,3 +1,9 @@
 return {
-    'ggandor/lightspeed.nvim'
+    'ggandor/lightspeed.nvim',
+    config=function ()
+        vim.api.nvim_set_keymap("n", "f", "f", { noremap=true })
+        vim.api.nvim_set_keymap("n", "F", "F", { noremap=true })
+        vim.api.nvim_set_keymap("n", "t", "t", { noremap=true })
+        vim.api.nvim_set_keymap("n", "T", "T", { noremap=true })
+    end
 }
