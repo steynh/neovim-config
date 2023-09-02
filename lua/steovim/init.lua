8eb17d98c7ab3c52a464d98252d721e69cb80137
Steyn Huurman <git@steynhuurman.nl>
2023-09-02 11:04:59 +0800

    feat: recognize zx files from shebang

--- lua/steovim/init.lua
+++ lua/steovim/init.lua
@@ -1,3 +1,19 @@
 require("steovim.lazy")
 require("steovim.remap")
 require("steovim.set")
+
+vim.filetype.add({
+    pattern = {
+        ['.*'] = {
+            priority=-math.huge,
+            function(_, bufnr)
+                local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1]
+                if (first_line == "#!/usr/bin/env zx") then
+                    return 'javascript'
+                end
+                return nil
+            end
+        }
+    },
+})
+
