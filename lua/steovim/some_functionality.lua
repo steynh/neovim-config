5271e9f8a1416fc42fed12399a5cdff919bf7a42
Steyn Huurman <git@steynhuurman.nl>
2023-09-02 22:02:46 +0800

    feat: better highlight and search

--- lua/steovim/some_functionality.lua
+++ lua/steovim/some_functionality.lua
@@ -26,5 +26,15 @@ function M.toggle_lsp_highlight()
     ]])
 end
 
+-- better highlight and search
+vim.keymap.set("n", "*", "*``", {})
+vim.on_key(function(key)
+  if vim.fn.mode() == "n" then
+      local char = vim.fn.keytrans(key)
+      local current_hlsearch = vim.opt.hlsearch:get()
+      local new_hlsearch = vim.tbl_contains({ "<CR>", "n", "N", "*", "#", "?", "/" }, char) or current_hlsearch and char == '`'
+      if current_hlsearch ~= new_hlsearch then vim.opt.hlsearch = new_hlsearch end
+  end
+end, vim.api.nvim_create_namespace "auto_hlsearch")
 
 return M
