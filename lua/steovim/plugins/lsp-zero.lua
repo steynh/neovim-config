8aff1bea8121b9b098a35a2a4e89d9629a55b60d
Steyn Huurman <git@steynhuurman.nl>
2023-09-02 11:08:41 +0800

    feat: enable mappings for nvim-cmp

--- lua/steovim/plugins/lsp-zero.lua
+++ lua/steovim/plugins/lsp-zero.lua
@@ -51,10 +51,10 @@ return {
         local cmp = require('cmp')
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
         local cmp_mappings = lsp.defaults.cmp_mappings({
-            --['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            --['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-            --['<C-y>'] = cmp.mapping.confirm({ select = true }),
-            --["<C-Space>"] = cmp.mapping.complete(),
+            ['<C-K>'] = cmp.mapping.select_prev_item(cmp_select),
+            ['<C-J>'] = cmp.mapping.select_next_item(cmp_select),
+            ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+            ["<C-i>"] = cmp.mapping.complete(),
         })
 
         cmp_mappings['<Tab>'] = nil
