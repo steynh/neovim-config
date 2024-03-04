45e086771d56e63fbeff6a4070073c7423a1bb3e
Steyn Huurman <git@steynhuurman.nl>
2024-03-04 15:01:58 +0800

    feat: add yank highlight feature from kickstart

--- lua/steovim/set.lua
+++ lua/steovim/set.lua
@@ -47,3 +47,12 @@ vim.api.nvim_create_autocmd({'FileType'}, {
     callback=function() vim.opt.formatoptions:remove('o') end
 })
 
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
