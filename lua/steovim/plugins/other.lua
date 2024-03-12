058c06940f30c9f6d37f22be93397103c0b4d51d
Steyn Huurman <git@steynhuurman.nl>
2024-03-12 17:51:43 +0800

    feat: improve viewing signatures

--- lua/steovim/plugins/other.lua
+++ lua/steovim/plugins/other.lua
@@ -1,3 +1,14 @@
 return {
-    {'junegunn/vim-easy-align'}
+    {'junegunn/vim-easy-align'},
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "VeryLazy",
+        opts = {
+            bind = true, -- This is mandatory, otherwise border config won't get registered.
+            handler_opts = {
+                border = "rounded"
+            }
+        },
+        hint_enable = false,
+    }
 }
