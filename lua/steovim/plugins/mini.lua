46ee2ce5f49a7b67c5217586979da5dd18976c1a
Steyn Huurman <git@steynhuurman.nl>
2024-03-21 21:06:33 +0800

    feat: add mini comment plugin

--- lua/steovim/plugins/mini.lua
+++ lua/steovim/plugins/mini.lua
@@ -21,6 +21,48 @@ return { -- Collection of various small independent plugins/modules
             }
         })
 
+        require('mini.comment').setup({
+            -- Options which control module behavior
+            options = {
+                -- Function to compute custom 'commentstring' (optional)
+                custom_commentstring = nil,
+
+                -- Whether to ignore blank lines
+                ignore_blank_line = false,
+
+                -- Whether to recognize as comment only lines without indent
+                start_of_line = false,
+
+                -- Whether to ensure single space pad for comment parts
+                pad_comment_parts = true,
+            },
+
+            -- Module mappings. Use `''` (empty string) to disable one.
+            mappings = {
+                -- Toggle comment (like `gcip` - comment inner paragraph) for both
+                -- Normal and Visual modes
+                comment = 'gk',
+
+                -- Toggle comment on current line
+                comment_line = 'gkk',
+
+                -- Toggle comment on visual selection
+                comment_visual = 'gk',
+
+                -- Define 'comment' textobject (like `dgc` - delete whole comment block)
+                -- Works also in Visual mode if mapping differs from `comment_visual`
+                textobject = 'gk',
+            },
+
+            -- Hook functions to be executed at certain stage of commenting
+            hooks = {
+                -- Before successful commenting. Does nothing by default.
+                pre = function() end,
+                -- After successful commenting. Does nothing by default.
+                post = function() end,
+            },
+        })
+
         -- Simple and easy statusline.
         --  You could remove this setup call if you don't like it,
         --  and try some other statusline plugin
